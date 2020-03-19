9c636e30ad9af1d65a8035863d6bcce9b0c46979
Stefan Oderbolz <stefan.oderbolz@liip.ch>
2020-03-19 14:50:11 +0100

    Add first version of scraper for BE

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-This is a scraper that runs on [Morph](https://morph.io). To get started [see the documentation](https://morph.io/documentation)
+Scraper for COVID-19 cases number in canton Bern
+=================================================
+
+This is a repo for a scraper of cantonal case numbers, it's based on the [#covid19mon hackathon challange](https://db.schoolofdata.ch/project/73).
+
+
--- scraper.py
+++ scraper.py
@@ -1,24 +1,122 @@
-# This is a template for a Python scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+# -*- coding: utf-8 -*-
 
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
+import requests
+from bs4 import BeautifulSoup
+import sqlite3
+import re
+import dateparser
+import traceback
 
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+DATABASE_NAME = 'data.sqlite'
+conn = sqlite3.connect(DATABASE_NAME)
+c = conn.cursor()
+c.execute(
+    '''
+    CREATE TABLE IF NOT EXISTS data (
+        date text,
+        area text,
+        tested  integer,
+        tested_pos  integer, 
+        confirmed integer,
+        time text,
+        deceased integer,
+        hospitalized integer,
+        recovered integer,
+        source text,
+        UNIQUE(date, time)
+    )
+    '''
+)
+conn.commit()
+
+
+def parse_page(soup, conn):
+    data = {
+        'date': None,
+        'area': 'Canton_BE',
+        'tested': None,
+        'tested_pos': None, 
+        'confirmed': None,
+        'time': '',
+        'deceased': None,
+        'hospitalized': None,
+        'recovered': None,
+        'source': 'https://www.besondere-lage.sites.be.ch/besondere-lage_sites/de/index/corona/index.html',
+    }
+
+    # parse number of confirmed cases and deceased
+    table = soup.find("h2", string=re.compile(".*Corona.*(e|E)rkrankungen.*Kanton.*Bern")).parent.find("table")
+    mapping = {
+         u'F\xe4lle': 'confirmed',
+         u'Todesf\xe4lle': 'deceased',
+    }
+    header = table.find_all('th')
+    cells = table.find_all('td')
+
+    assert len(header) == 2, "The number of headers changed"
+    assert len(cells) == 2, "The number of cells changed"
+
+    for i, value in enumerate(header):
+        data[mapping[value.string]] = int(cells[i].string)
+
+    # parse the date
+    date_str = re.search(": (.*)\)", soup.find(string=re.compile(".*Stand:.*")).string).group(1)
+    update_datetime = dateparser.parse(
+        date_str,
+        languages=['de']
+    )
+    data['date'] = update_datetime.date().isoformat()
+    c = conn.cursor()
+
+    try:
+        c.execute(
+            '''
+            INSERT INTO data (
+                date,
+                time,
+                area,
+                tested,
+                tested_pos, 
+                confirmed,
+                deceased,
+                hospitalized,
+                recovered,
+                source
+            )
+            VALUES
+            (?,?,?,?,?,?,?,?,?,?)
+            ''',
+            [
+                data['date'],
+                data['time'],
+                data['area'],
+                data['tested'],
+                data['tested_pos'],
+                data['confirmed'],
+                data['deceased'],
+                data['hospitalized'],
+                data['recovered'],
+                data['source'],
+            ]
+        )
+    except sqlite3.IntegrityError:
+        print("Error: Data for this date + time has already been added")
+    finally:
+        conn.commit()
+    
+
+# canton bern - start url
+start_url = 'https://www.be.ch/corona'
+
+# get page with data on it
+page = requests.get(start_url)
+soup = BeautifulSoup(page.content, 'html.parser')
+
+try:
+    parse_page(soup, conn)
+except Exception as e:
+    print("Error: %s" % e)
+    print(traceback.format_exc())
+    raise
+finally:
+    conn.close()
